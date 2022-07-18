d8d6cba27a98993ff31cf2c3b3e82cd63df91404
MissLiner <miss.liner@yahoo.com>
2022-07-18 17:02:48 -0400

    Install node-sass

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import './App.css';
-import bootstrap from 'bootstrap';
-
 function App() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <h1 className="navbar-brand">Spell!</h1>
-
-
-
     </nav>
   )
 }
