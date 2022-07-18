fe00824524230f0f9d1e81af98e7998758bd8fa0
MissLiner <miss.liner@yahoo.com>
2022-07-18 16:14:03 -0400

    Clear react app boilerplate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import bootstrap from 'bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <h1 className="navbar-brand">Spell!</h1>
 
+
+
+    </nav>
+  )
+}
 export default App;
