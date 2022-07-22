c8881b4280702bdb222bcef46cad606014495410
MissLiner <miss.liner@yahoo.com>
2022-07-22 16:18:19 -0400

    Add three more words

--- src/data/card_data.js
+++ src/data/card_data.js
@@ -1,6 +1,9 @@
 import catPic from 'images/pexels-krysten-merriman-20787.jpg';
 import dogPic from 'images/pexels-kat-smith-551628.jpg';
 import wolfPic from 'images/pexels-steve-682361.jpg';
+import mousePic from 'images/pexels-pixabay-301448.jpg';
+import pigPic from 'images/pexels-mali-maeder-110820.jpg';
+import frogPic from 'images/pexels-thierry-fillieul-1046494.jpg'
 import loadPic from 'images/loading.png';
 import winPic from 'images/win.png';
 
@@ -25,7 +28,28 @@ const card_data = [
   pic: wolfPic,
   credit: [ "Photo by Steve from Pexels", 
             "https://www.pexels.com/collections/spell-animals-7xnhrmk/"]
-  }
+  },
+  { name: "mouse",
+  letters: ['m', 'o', 'u', 's', 'e'],
+  lCount: 5,
+  pic: mousePic,
+  credit: [ "Photo by Pixabay from Pexels", 
+            "https://www.pexels.com/photo/animal-cute-little-mouse-301448/"]
+  },
+  { name: "pig",
+  letters: ['p', 'i', 'g'],
+  lCount: 3,
+  pic: pigPic,
+  credit: [ "Photo by mali maeder from Pexels", 
+            "https://www.pexels.com/photo/white-pig-110820/"]
+  },
+  { name: "frog",
+  letters: ['f', 'r', 'o', 'g'],
+  lCount: 4,
+  pic: frogPic,
+  credit: [ "Photo by Thierry Fillieul from Pexels", 
+            "https://www.pexels.com/photo/closeup-photo-of-blue-frog-on-green-surface-1046494/"]
+  },
   ]
   const start_card = {
     name: "start",
